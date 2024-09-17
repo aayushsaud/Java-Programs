ee6d80bd4bbc3f6b8b98fb0f049d8a2dc686af17
aayushsaud <saudaayush.dev@gmail.com>
2024-09-17 20:47:09 +0545

    Check if Palindrome

---
+++ control_flow_programs/Check_if_Palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Check_if_Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking user to enter a string
+        System.out.print("Enter a word: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+        
+        // Reversing the String
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        System.out.println(input + reversed);
+
+        // Checking if the String is a Palindrome
+        if (input.equals(reversed))System.out.println(input + " is a Palindrome!");
+        else System.out.println(input + " is not a Palindrome!");
+
+        scanner.close();
+    }
+}
