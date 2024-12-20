d1f9555a66f0e1e8889d3afff8bd5e8739d45e3e
aayushsaud <saudaayush.dev@gmail.com>
2024-12-20 13:36:30 +0545

    Insertion Sort Algorithm

---
+++ arrays_and_strings/Insertion_Sort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Insertion_Sort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        
+        int[] array = new int[n];
+
+        // Adding elements in the array
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the " + i + " element of the array: ");
+            array[i] = scanner.nextInt();
+        }
+
+       // Insertion sort algorithm
+        for (int i = 1; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && array[j - 1] > array[j]) { // Comparing the current element with the previous one
+                int temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+                j--;
+            }
+        }
+        System.out.println("Sorted array: " + Arrays.toString(array));
+
+        scanner.close();
+    }
+}
