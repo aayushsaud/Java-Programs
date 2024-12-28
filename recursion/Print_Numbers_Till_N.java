a6b58b30bddfb19242405a7b3b82ee36fc7472fe
aayushsaud <saudaayush.dev@gmail.com>
2024-12-28 14:15:25 +0545

    Print Numbers Till N

---
+++ recursion/Print_Numbers_Till_N.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Print_Numbers_Till_N {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompting the user for the number till which the program needs to print
+        System.out.print("Enter a Number: ");
+        int number = scanner.nextInt();
+
+        printNumbersTill(number);
+
+        scanner.close();
+    }
+    public static void printNumbersTill(int number) {
+        if (number <= 0) return;
+        printNumbersTill(number - 1); // Back-tracking prints from 1
+        System.out.println(number);
+    }
+}
