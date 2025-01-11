c75b1f74b2f387507910a7f31b6b39c2a155f791
aayushsaud <saudaayush.dev@gmail.com>
2025-01-11 12:01:58 +0545

    Largest and Smallest Element in Array

--- arrays/largest_and_smallest_in_array.java
+++ arrays/largest_and_smallest_in_array.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class largest_and_smallest_in_array {
+public class Largest_and_Smallest_in_Array {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -9,6 +9,12 @@ public class largest_and_smallest_in_array {
         // Asking the user for the size of the array
         System.out.print("Enter the size of the array: ");
         int size = scanner.nextInt();
+
+        if (size <= 0) {
+            System.out.println("Invalid array size. Please enter a positive integer.");
+            scanner.close();
+            return;
+        }
 
         int[] array = new int[size];
 
@@ -18,21 +24,17 @@ public class largest_and_smallest_in_array {
             array[i] = scanner.nextInt();
         }
 
-        System.out.println();
-
-        // Printing the given array
-        System.out.println("Your Array: " + Arrays.toString(array));
-
-        System.out.println();
+        System.out.println("\nYour Array: " + Arrays.toString(array) + "\n");
 
         int smallest = array[0];
         int largest = array[0];
-        
-        // Adding up the elements in the array
-        for (int i = 0; i < size; i++) {
+
+        // Finding the smallest and largest numbers
+        for (int i = 1; i < size; i++) { // Start from index 1
             if (array[i] < smallest) smallest = array[i];
             if (array[i] > largest) largest = array[i];
         }
+
         System.out.println("The smallest number in your array is: " + smallest);
         System.out.println("The largest number in your array is: " + largest);
 
